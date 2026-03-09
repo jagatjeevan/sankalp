baef0fea8cd45a66f69dd4a5b7e297557dd2fe67
Jagat Jeevan Sahoo <kanhajeevan@gmail.com>
2026-03-10 00:27:56 +0530

    Update: Remove the draggable theme

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -1,13 +1,7 @@
 'use client';
 
 import { ReactNode } from 'react';
-import { ThemeSwitcher } from '@/components/ThemeSwitcher';
 
 export function AppShell({ children }: { children: ReactNode }) {
-  return (
-    <>
-      {children}
-      <ThemeSwitcher />
-    </>
-  );
+  return <>{children}</>;
 }
